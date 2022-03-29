db83c09e3e1a3039d1f930db4f9f900c778c1b2b
Mathieu <mathieu.linder.dev@gmail.com>
2022-03-29 23:22:53 +0200

    part3 tuto

--- 12-http.js
+++ 12-http.js
@@ -13,6 +13,6 @@ const server = http.createServer((req, res) => {
             <p>We can't seem to find the page you are looking for</p>
             <a href="/">back home</a>
          `)}
-})
+});
 
 server.listen(5000);
