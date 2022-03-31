1c7b778d007eb98f30ac09e01c4929ad790e773e
Mathieu <mathieu.linder.dev@gmail.com>
2022-03-31 19:27:33 +0200

    part7 tuto

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,18 @@
-const http = require('http')
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
+// setup static and middleware
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
-server.listen(5000)
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+app.listen(5000, () => {
+  console.log(`Server is listening on port 5000...`);
+})
