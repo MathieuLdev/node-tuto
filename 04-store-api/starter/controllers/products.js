758393b8d957ff7e9e85345d9e845b5e6f69a741
Mathieu <mathieu.linder.dev@gmail.com>
2022-04-08 22:45:33 +0200

    store api part2

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,12 +3,26 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
 	// throw new Error('Testing async errors')
 	const products = await Product.find({
-		name: 'vase table',
+		name: "vase table",
 	});
 	res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-	res.status(200).json({ msg: "products route" });
+	const { featured, company, name } = req.query;
+	const queryObject = {};
+
+	if (featured) {
+		queryObject.featured = featured === "true" ? true : false;
+	}
+	if (company) {
+		queryObject.company = company;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: "i" };
+	}
+	console.log(queryObject);
+	const products = await Product.find(queryObject);
+	res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
