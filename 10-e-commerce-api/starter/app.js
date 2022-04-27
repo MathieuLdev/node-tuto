30069cc542fcab27db440bf3f3dc916acc11ea13
Mathieu <mathieu.linder.dev@gmail.com>
2022-04-27 19:23:46 +0200

    10-e-commerce-api part 4

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,26 +1,27 @@
 require("dotenv").config();
-require('express-async-errors');
+require("express-async-errors");
 
 // express
 const express = require("express");
 const app = express();
 
 // rest of the packages
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 // database
 const connectDB = require("./db/connect");
 
 // routers
-const authRouter = require('./routes/authRoutes');
-const userRouter = require('./routes/userRoutes')
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
@@ -32,8 +33,9 @@ app.get("/api/v1", (req, res) => {
 	res.send("e-commerce-api");
 });
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
