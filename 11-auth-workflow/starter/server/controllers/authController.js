dddcc2fccaa9e109cc3b91f5d4c8be77a5ba8d90
Mathieu <mathieu.linder.dev@gmail.com>
2022-05-05 23:34:13 +0200

    11-auth-workflow part2

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -15,7 +15,7 @@ const register = async (req, res) => {
 	// first registered user is an admin
 	const isFirstAccount = (await User.countDocuments({})) === 0;
 	const role = isFirstAccount ? "admin" : "user";
-	const verificationToken = crypto.randomBytes(40).toString('hex');
+	const verificationToken = crypto.randomBytes(40).toString("hex");
 	const user = await User.create({
 		name,
 		email,
@@ -29,6 +29,25 @@ const register = async (req, res) => {
 		verificationToken: user.verificationToken,
 	});
 };
+
+const verifyEmail = async (req, res) => {
+	const { verificationToken, email } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new CustomError.UnauthenticatedError("Verification failed");
+	}
+	if (user.verificationToken !== verificationToken) {
+		throw new CustomError.UnauthenticatedError("Verification failed");
+	}
+	user.isVerified = true;
+	user.verified = Date.now();
+	user.verificationToken = "";
+
+	await user.save();
+
+	res.status(StatusCodes.OK).json({ msg: "Email Verified" });
+};
+
 const login = async (req, res) => {
 	const { email, password } = req.body;
 
@@ -53,6 +72,7 @@ const login = async (req, res) => {
 
 	res.status(StatusCodes.OK).json({ user: tokenUser });
 };
+
 const logout = async (req, res) => {
 	res.cookie("token", "logout", {
 		httpOnly: true,
@@ -65,4 +85,5 @@ module.exports = {
 	register,
 	login,
 	logout,
+	verifyEmail,
 };
