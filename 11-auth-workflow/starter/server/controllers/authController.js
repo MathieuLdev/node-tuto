2f121a279957f2f8ff2c7414aff1bf65afdd03a9
Mathieu <mathieu.linder.dev@gmail.com>
2022-05-06 01:32:07 +0200

    11-auth-workflow part3

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -1,7 +1,11 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const { attachCookiesToResponse, createTokenUser } = require("../utils");
+const {
+	attachCookiesToResponse,
+	createTokenUser,
+	sendVerificationEmail,
+} = require("../utils");
 const crypto = require("crypto");
 
 const register = async (req, res) => {
@@ -23,10 +27,23 @@ const register = async (req, res) => {
 		role,
 		verificationToken,
 	});
+	const origin = "http://localhost:3000";
+
+	// const tempOrigin = req.get("origin");
+	// const protocol = req.protocol;
+	// const host = req.get("host");
+	// const forwardedHost = req.get("x-forwarded-host");
+	// const forwardedProtocol = req.get("x-forwarded-proto");
+
+	await sendVerificationEmail({
+		name: user.name,
+		email: user.email,
+		verificationToken: user.verificationToken,
+		origin,
+	});
 	// send verification token back only while testing in postman !
 	res.status(StatusCodes.CREATED).json({
 		msg: "Success ! Please check your email to verify the account",
-		verificationToken: user.verificationToken,
 	});
 };
 
---
+++ 11-auth-workflow/starter/server/utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+	host: "smtp.ethereal.email",
+	port: 587,
+	auth: {
+		user: "karson.reynolds58@ethereal.email",
+		pass: "QPsPpEuveKd7zD66Xq",
+	},
+};
